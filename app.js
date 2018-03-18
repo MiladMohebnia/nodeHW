9dd73f035dab0eedcebd4cbdb940dc08be87355c
milad mohebnia <miladmohebnia@gmail.com>
2018-03-18 12:39:51 +0330

    first commit

--- app.js
+++ app.js
@@ -15,4 +15,4 @@ app.get( "/", (req, res)=>{
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-app.listen( server_port, server_ip_address, _=>{ console.log( "server is running!" ) } );
+app.listen( server_port, server_ip_address, _=>{ console.log( `server is running! on ${server_ip_address}:${server_port}` ) } );
